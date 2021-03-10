cb8a444325acda7c3c09bbfb59d2c6acea154efa
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2021-03-10 12:12:33 -0800

    rm files

--- pair_15o.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm parallel_pair_gale_shapley_hdf5
-make parallel_pair_gale_shapley_hdf5
-export HDF5_USE_FILE_LOCKING=FALSE
-echo "./parallel_pair_gale_shapley_hdf5 15o_246001.root 15o_246001.hdf5 $1 $2 0"
-./parallel_pair_gale_shapley_hdf5 15o_246001.root 15o_246001.hdf5 $1 $2 0
