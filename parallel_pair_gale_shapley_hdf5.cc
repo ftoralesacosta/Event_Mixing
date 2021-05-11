0c45e3a55ea95c2af0b1939e8b221365f98678ba
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2021-05-11 14:16:22 -0700

    fixed v2 bug. MP normalization bug remains

--- parallel_pair_gale_shapley_hdf5.cc
+++ parallel_pair_gale_shapley_hdf5.cc
@@ -109,7 +109,8 @@ namespace {
 
 				if (nfeature >= 3)
 				  ret.push_back(event_plane_angle[1]);
-        //fprintf(stderr,"%d: z-vertex = %1.2f mp = %f, v2 = %1.2f\n",__LINE__,vertex[2],multp_sum,event_plane_angle[1]);
+        if (i > event_start - 3)
+        fprintf(stderr,"%s: %d: z-vertex = %1.2f mp = %f, v2 = %1.2f\n",__func__,__LINE__,vertex[2],multp_sum,event_plane_angle[1]);
 			}
 
 		}
@@ -152,7 +153,7 @@ namespace {
 	{
 
 	  //HAVE THE BLOCK SIZE BE EVENT_END-EVENT_START, AND PUT OFFSET AS EVEST_START
-	  fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, "Befor HDF5 READ");
+	  /* fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, "Befor HDF5 READ"); */
 	  std::string file_str = filename;
 
     const int EVENT_RANK = 2; // Rank 2 because [No. Events, Event Variables]
@@ -175,8 +176,8 @@ namespace {
 
 		const hsize_t block_size = event_end-event_start;
 		float event_data_out[block_size][NEvent_Vars] = {0};
-		fprintf(stderr,"%s:%d: HDF5 Block Size = %i, Event Start = %i, NVars = %i\n",
-			__FILE__,__LINE__,block_size,event_start,NEvent_Vars);
+		/* fprintf(stderr,"%s:%d: HDF5 Block Size = %i, Event Start = %i, NVars = %i\n", */
+		/* 	__FILE__,__LINE__,block_size,event_start,NEvent_Vars); */
 
 		hsize_t event_offset[EVENT_RANK] = {event_start,0};
 		hsize_t event_count[EVENT_RANK] = {block_size, NEvent_Vars};
@@ -188,7 +189,7 @@ namespace {
 		event_dataspace.selectHyperslab( H5S_SELECT_SET, event_count, event_offset );
 		event_memspace.selectHyperslab( H5S_SELECT_SET, event_count_out, event_offset_out );
 		event_dataset.read( event_data_out, H5::PredType::NATIVE_FLOAT, event_memspace, event_dataspace);
-		fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, "event dataset read into array: OK");
+		/* fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, "event dataset read into array: OK"); */
 
 		std::vector<float> ret;
 
@@ -199,50 +200,66 @@ namespace {
 		    float multp = event_data_out[i][1];
 		    ret.push_back(multp);
 		    if (nfeature >=3){
-          float v2 = event_data_out[i][3];
+          float v2 = event_data_out[i][2];
           ret.push_back(v2);//index 2 is v1, 3 is v2, 4 is v3
-          //fprintf(stderr,"%d: z-vertex = %1.2f mp = %f, v2 = %1.2f\n",__LINE__,z_vtx,multp,v2);
+          if (std::isnan(v2)||v2==0)
+            fprintf(stderr,"%s: %d: NAN or 0 v2; i = %zu, z-vertex = %1.2f mp = %f, v2 = %1.2f\n",__func__,__LINE__,i,z_vtx,multp,v2);
+          if (i==0)
+            fprintf(stderr,"%s: %d: z-vertex = %1.2f mp = %f, v2 = %1.2f\n",__func__,__LINE__,z_vtx,multp,v2);
           }
 		  }
 		}
 
-//    for (int i = 0; i < ret.size();i+=3){
-//        fprintf(stderr,"%d: size =  %i, z-vertex = %1.2f mp = %f, v2 = %1.2f\n",
-//            __LINE__,ret.size(),ret[i+0],ret[i+1],ret[i+2]);
-//        }
+    for (int i = 0; i < ret.size();i+=3){
+      if (i < ret.size() - 3) continue;
+      fprintf(stderr,"%d: size =  %i, z-vertex = %1.2f mp = %f, v2 = %1.2f\n",
+          __LINE__,ret.size(),ret[i+0],ret[i+1],ret[i+2]);
+    }
 		return ret;
 	}
   
 	void feature_normalize(std::vector<float> &u,
 			       std::vector<float> &v, const size_t n)
-	{
-	  //normalize vectors such that event properties with very different magnitudes can be equally weighted in pairing
+  {
+    //normalize vectors such that event properties with very different magnitudes can be equally weighted in pairing
     //ex: multiplicity can range from 0-1,000. But z-vertex only ranges from 0-10. Normalization is needed to pair
     //events with both criteria simoultaneously.
+    /* "n" is the number of features. Likely to be 3: z-vert, multp, and v2 */
 
+    //sum each event property for each vector (datasets)
     std::vector<float> s(n, 0);
     for (size_t j = 0; j < n; j++) {
       float s_j = 0;
 
       for (size_t i = 0; i < u.size(); i += n) {
         s_j += fabsf(u[i + j]);
+        /* if (j==1 && i%500 == 0) */
+        /*   fprintf(stderr,"%s: %d: i = %zu, mp_sum = %f\n",__func__,__LINE__,i/3,s_j); */
+        /* if (i/3 > 1997) */
+        /*   fprintf(stderr,"%s: %d: i = %zu z = %f, mp = %f, v2 = %f \n",__func__, __LINE__,i/3,u[i+0],u[i+1],u[i+2]); */
       }
       s[j] = s_j;
     }
+
     for (size_t j = 0; j < n; j++) {
       float s_j = 0;
 
       for (size_t i = 0; i < v.size(); i += n) {
         s_j += fabsf(v[i + j]);
+        /* if (j==0 && i/3>1997){ */
+        /*   fprintf(stderr,"%s: %d: i = %i, z = %f, mp = %f, v2 = %f \n",__func__,__LINE__,i/3,v[i+0],v[i+1],v[i+2]); */
+          /* fprintf(stderr,"%s: %d: v[i + j] = %f\n",__func__,__LINE__,v[i + j]); */
+        /* } */
       }
       s[j] += s_j;
-    }//sum each event property for each vector
+    }
 
+    //get normalization factor: N_Events/property_sum
     for (size_t j = 0; j < n; j++) {
       s[j] = (u.size() + v.size()) / s[j];
+    }
 
-    }//get normalization factor: N_Events/sum
-
+    //apply normalization to each vector. you want the same normalization applied to each dataset.
     for (size_t i = 0; i < u.size(); i += n) {
       for (size_t j = 0; j < n; j++) {
         u[i + j] *= s[j];
@@ -253,7 +270,8 @@ namespace {
       for (size_t j = 0; j < n; j++) {
         v[i + j] *= s[j];
       }
-    }//apply normalization to each vector
+    }
+
   }
 }
 
@@ -281,15 +299,20 @@ void order_preference(std::vector<std::list<index_t> > &up,
   //Loopt through each event in dataset_0 block
   for (size_t i = 0; i < u_size_n; i++) {
     std::vector<std::pair<float, index_t> > l;
-
+    /* if (i==0) */
+      /* fprintf(stderr,"%s: %d:[i] z = %f, mp = %f, v2 = %f\n",__func__,__LINE__,u[i*n+0],u[i*n+1],u[i*n+2]); */
     for (size_t j = 0; j < v_size_n; j++) {
       float d = 0;
+
+      /* if (j==0 && i==0) */
+        /* fprintf(stderr,"%s: %d:[j] z = %f, mp = %f, v2 = %f\n",__func__,__LINE__,v[i*n+0],v[i*n+1],v[i*n+2]); */
 
       for (size_t k = 0; k < n; k++)//loop through featurs for distance metric d
         d += std::pow(u[i * n + k] - v[j * n + k], 2);
       if (d==0) d = 999999; //Avoid pairing identical events
       l.push_back(std::pair<float, size_t>(d, j));
-//      fprintf(stderr,"%d: Distance = %f\n",__LINE__,d);
+      /* if (i%1000 == 0 || j%1000 == 0); */
+      /* fprintf(stderr,"%d: Distance = %f\n",__LINE__,d); */
     }
     std::sort(l.begin(), l.end(), preference_compare);
     // up.push_back(std::list<index_t>());
@@ -300,13 +323,13 @@ void order_preference(std::vector<std::list<index_t> > &up,
       }
     }
     up[i].resize(size_max, v_size_n);
-     //if (i % 100 == 0) {
-     //  fprintf(stderr, "%s:%d: %lu/%lu\n", __FILE__, __LINE__,i, u_size_n);
-     //}
+    //if (i % 100 == 0) {
+    //  fprintf(stderr, "%s:%d: %lu/%lu\n", __FILE__, __LINE__,i, u_size_n);
+    //}
   }
 
   vp.resize(v_size_n * nduplicate_v, std::list<index_t>());
- 
+
   //Loop through each event in dataset_1 block
   for (size_t j = 0; j < v_size_n; j++) {
     std::vector<std::pair<float, index_t> > l;
@@ -337,12 +360,14 @@ void order_preference(std::vector<std::list<index_t> > &up,
     //fprintf(stderr, "%s:%d: %lu/%lu\n", __FILE__, __LINE__,j, v_size_n);}
   }
 
-  fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, "Order Preference done for this block");
+  /* fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, "Order Preference done for this block"); */
 }
 
 std::vector<index_t> gale_shapley(std::vector<std::list<index_t> > &mp,
     std::vector<std::list<index_t> > &fp)
 {
+  /* pass in male and female preference lists. Create vectors to track */ 
+  /* which males are engaged to which females and vice-versa. */
   std::vector<index_t> m_to_f_engaged(mp.size(), fp.size());
   std::vector<index_t> f_to_m_engaged(fp.size(), mp.size());
 
@@ -352,6 +377,7 @@ std::vector<index_t> gale_shapley(std::vector<std::list<index_t> > &mp,
 
   mp_index.resize(fp.size());
 
+  /* iterate through all males (outer), and then that male's preference list (inner) */
   for (std::vector<std::list<index_t> >::iterator
       iterator_outer = mp.begin();
       iterator_outer != mp.end(); iterator_outer++) {
@@ -371,7 +397,7 @@ std::vector<index_t> gale_shapley(std::vector<std::list<index_t> > &mp,
     //if ((iterator_outer - mp.begin()) % 100 == 0) {
     //  fprintf(stderr, "%s:%d: %lu/%lu\n", __FILE__, __LINE__, 
     //      (iterator_outer - mp.begin()), mp.size()); }
-     
+
 
   }
 
@@ -387,16 +413,16 @@ std::vector<index_t> gale_shapley(std::vector<std::list<index_t> > &mp,
     const index_t m = m_iterator - m_to_f_engaged.begin();
     const index_t w = mp[m].front();
 
-     //if (m % 100 == 0 || w % 100 == 0) {
-     //fprintf(stderr, "%s:%d: %hu<>%hu\n", __FILE__, __LINE__,m, w);
-     //}
+    /* if (m % 500 == 0 || w % 500 == 0) */
+    /* fprintf(stderr, "%s:%d: %hu<>%hu\n", __FILE__, __LINE__,m, w); */
+
 
     // Some man p is engaged to w
     index_t p = f_to_m_engaged[w];
 
     if (p != mp.size()) {
       // Assign p to be free
-      m_to_f_engaged[p] = fp.size();
+      m_to_f_engaged[p] = fp.size(); //clever way of assigning non-existent fp element index (fp index are 0 through size-1)
     }
     f_to_m_engaged[w] = m;
     m_to_f_engaged[m] = w;
@@ -405,9 +431,13 @@ std::vector<index_t> gale_shapley(std::vector<std::list<index_t> > &mp,
       std::find(fp[w].begin(), fp[w].end(), m);
 
     s++;
-    for (std::vector<std::pair<std::vector<std::list<index_t> >::
-        iterator, std::list<index_t>::iterator> >::iterator
-        iterator = mp_index[w].begin();
+
+    //once paired, delete partners lower on preference list -> faster subsequent pairings
+
+    /* for (std::vector<std::pair<std::vector<std::list<index_t> >:: */
+    /*     iterator, std::list<index_t>::iterator> >::iterator */
+    /*     iterator = mp_index[w].begin(); */
+    for (auto iterator = mp_index[w].begin();
         iterator != mp_index[w].end(); iterator++) {
       iterator->first->erase(iterator->second);
     }
@@ -431,69 +461,92 @@ std::map<size_t,std::vector<Long64_t> > mix_gale_shapley(const char *filename_0,
   size_t mix_end = atoi(mixing_end);
   int Track_Skim = atoi(GeV_Track_Skim);
 
-  const size_t nevent_0 = nevent(filename_0);
+  /* const size_t nevent_0 = nevent(filename_0); */
+  size_t nevent_0 = nevent_hdf5(filename_0);
   size_t nevent_1 = nevent_hdf5(filename_1);
 
   fprintf(stderr,"\n N EVENTS IN %s = %u \n",filename_0,nevent_0);
 
   size_t block_size = 2000;
+  /* size_t block_size = 1999; */
   // block size is the size of the "pool" events are mixed in. 2000 is a good start as it reaches a stable
   // pairing solution with events that are very similar. Block size can be reduced to 1000 if limited N events.
 
   int max_remainder_events = block_size/4;
-  //max number of events not used if nevents/block_size don't divide evenly.  
+  //max number of events not used if nevents/block_size don't divide evenly. "N/4" is somewhat arbitrary 
+
   while (nevent_0 % block_size > max_remainder_events){
     block_size --;
-    //ensure n_events/blocksize doesn't have too large of a remainder.
+    //ensure n_events/blocksize isn't too large. This will correspond to triggered events
+    //at the end of the root file without pairings.
   }
 
   /* const size_t nblocks_0 = 2; */
   const size_t nblocks_0 = nevent_0 / block_size;
-  const size_t nblocks_1 = ((nevent_1 * nduplicate)/block_size);
-
-  //For even distribution of MB events
-  int remainder_1 = (nevent_1 % block_size)/(mixing_end-mixing_start);
-
   std::vector<std::vector<float> >feature_0_vec;
-  std::vector<std::vector<float> >feature_1_vec;
 
   //events from dataset_0 (usually the triggered dataset) make up the "block" structure. The number of
   // blocks is roughly Nevents/block_size. Each block of dataset_0 is eventually mixed with "n_mix" blocks of
   // dataset_1 (MB/other data), such that each triggered event has n_mix pairings, and each event pairing 
   // comes from a unique block-pairing.
-  //
-  // Example: n_mix = 300, blocksize = 2000. The first 2000 events (according to the block size) of dataset0
-  // will each get 300 event-pairing by applying the gale shapeley algorithm to dataset_0:dataset_1 block-pairs
-  // 0:0, 0:1, 0:2, ...0:299. Repeat by incrementing dataset_0 blocks.
+
   for(size_t h = 0; h < nblocks_0; h++){
 
     size_t event_start_0 = h * block_size;
     size_t event_end_0 = event_start_0 + block_size;
 
-    feature_0_vec.push_back(feature_extract(filename_0, event_start_0, event_end_0,nfeature));
+    feature_0_vec.push_back(feature_extract_hdf5(filename_0, event_start_0, event_end_0,nfeature));
+    /* feature_0_vec.push_back(feature_extract(filename_0, event_start_0, event_end_0,nfeature)); */
 
-    fprintf(stderr,"\n %d: GAMMA EVENT START=%u || EVENT END=%u",
+    fprintf(stderr,"%d: GAMMA EVENT START=%u || EVENT END=%u\n",
         __LINE__,event_start_0,event_end_0);
   }
 
-  //n_mix dictates the number of blocks in MB data. So, each mixing iterator, i, indicates an
+  //n_mix dictates the number of blocks in MB data, since events in the second dataset can be reused for different
+  //events in the first dataset.. So, each mixing iterator, i, indicates an
   //iteration through n_mix blocks of MB data. Each and every triggered block of events is mixed
   //with n_mix blocks of MB data.
+
+  size_t nblocks_1 = mix_end-mix_start;
+  fprintf(stderr,"\n%d: mix end = %zu, mix start = %zu\n",__LINE__,mix_end,mix_start);
+  /* const size_t nblocks_1 = ((nevent_1 * nduplicate)/block_size); */
+
+  /* if (nblocks_1 < ((nevent_1 * nduplicate)/block_size)){ */
+  /*   fprintf(stderr,"Not enough events in dataset 1 (hdf5 file) to mix %zu blocks at a blocksize of %zu \n",nblocks_1,block_size); */
+  /*   exit(EXIT_FAILURE); */
+  /*   /1* FIXME: add in a while loop to iterate the blocksize, as we did above *1/ */
+  /* } */
+
+  //For even distribution of MB events
+  //assumes dataset_1 (min-bias) is larger than triggered dataset, and spreads remainder throughout the dataset
+  //Note: remainder is only used here. Afterwards, the block of data is taken from the mixing index in the main loop
+  //where consecutive blocks of dataset_1 are alreay separated by this remainder.
+  int remainder_1 = (nevent_1-block_size*nblocks_1)/nblocks_1;
+  remainder_1=0;
+  fprintf(stderr,"%d: nblocks_1 = %zu, remainder_1 = %i\n",__LINE__,nblocks_1,remainder_1);
+
+  std::vector<std::vector<float> >feature_1_vec;
+
   for (size_t i = mix_start; i < mix_end; i++) {
 
     size_t event_start_1 = i * (block_size + remainder_1);
+    /* size_t event_start_1 = i * nevent_1 / (nblocks_1);  //old */
     size_t event_end_1 = event_start_1 + block_size;
-    //assumes dataset_1 (min-bias) is larger than triggered dataset,and spreads remainder throughout the dataset
+    /* size_t event_start_1 = i * nevent_1 / (nblocks_1+1); */
+    /* size_t event_end_1 = (i + nduplicate) * nevent_1 / */
+    /*       (nblocks_1+ nduplicate); */
 
-    fprintf(stderr,"\n %d: MB EVENT START=%u || EVENT END=%u \n",
+    fprintf(stderr,"%d: MB EVENT START=%u || EVENT END=%u \n",
         __LINE__,event_start_1,event_end_1);   
 
     feature_1_vec.push_back(feature_extract_hdf5(filename_1, event_start_1, event_end_1,nfeature));
   }
 
+  /* fprintf(stderr,"%d: Number of Threads = %i",__LINE__,omp_get_num_threads()); */
 #pragma omp parallel for ordered schedule(dynamic)
+  //each thread will independently pair data_0 a single block with all [n_mix] blocks from data_1.
+  //all relevant data are at this point in thread-safe vectors, root is no longer used.
 
-  //each thread will independently pair data_0 blocks with n_mix blocks from data_1
   for(size_t h = 0; h < nblocks_0; h++){
 
     fprintf(stderr,"%s:%d: %s %lu %s %lu\n",__FILE__,__LINE__,"Block",h,"of",nblocks_0);	   
@@ -501,12 +554,15 @@ std::map<size_t,std::vector<Long64_t> > mix_gale_shapley(const char *filename_0,
     size_t event_start_0 = h * block_size;
     std::vector<std::vector<Long64_t> > k;		  
 
+    std::vector<float> feature_0_scaled = feature_0_vec[h];
+
     for (size_t i = mix_start; i < mix_end; i++) {
 
-      std::vector<float> feature_0_scaled = feature_0_vec[h];
+      //normalize datasets
       std::vector<float> feature_1_scaled = feature_1_vec[i];
-
       feature_normalize(feature_0_scaled, feature_1_scaled,nfeature);
+
+      //create preference list
       std::vector<std::list<index_t> > preference_0;
       std::vector<std::list<index_t> > preference_1;
 
@@ -514,16 +570,33 @@ std::map<size_t,std::vector<Long64_t> > mix_gale_shapley(const char *filename_0,
           feature_1_scaled, nfeature, nduplicate);
 
       std::vector <index_t> m;
-
       m = gale_shapley(preference_0, preference_1);
 
-      const size_t feature_1_size_nfeature = feature_1_vec[i].size() / nfeature;
 
-      size_t event_start_1 = i * nevent_1 / (nblocks_1+1);
+      const size_t feature_1_size_nfeature = feature_1_vec[i].size() / nfeature; //basically block_size_1
+      /* fprintf(stderr,"\n\n %s:%d feature_1_size_nfeature = %zu\n",__FILE__,__LINE__,feature_1_size_nfeature); */
+
+      size_t event_start_1 = i * (block_size + remainder_1);
+      size_t event_end_1 = event_start_1+block_size; 
+      /* size_t event_start_1 = i * nevent_1 / (nblocks_1+1); */
+      /* size_t event_end_1 = (i + nduplicate) * nevent_1 / */
+      /*     (nblocks_1+ nduplicate); */
+      /* size_t event_start_1 = i * nevent_1 / (nblocks_1);  //old */
+
+      //output of m = gale_shapley are consecutive elements of dataset_1. Need to convert back to raw event#
+      /* fprintf(stderr," %s:%d event_start_1 for mixing map output = %zu; Next = %zu\n\n",__FILE__,__LINE__,event_start_1,event_end_1); */
       std::vector <Long64_t>tempflat;
 
+      //This basically restructures the output,
+      //such that all the mixed events for the same triggered event 
+      //are together in a vector of dimension: [n_events][n_mix]
+      //
       for (size_t j = 0; j < m.size(); j++) {                                        
+        //following line is crucial: converts element index in m = gale_shapeley
+        //to raw event number from dataset_1 ("MB" dataset). Since m only is filled
+        //with elements, the correct event start for that block is added).
         Long64_t q = event_start_1 + (m[j] % feature_1_size_nfeature);
+        /* fprintf(stderr,"%d: Index = %zu, Event Start = %zu, m[j] = %zu, Mixed Event q  = %zu\n",__LINE__,i,event_start_1,m[j] % feature_1_size_nfeature, q); */
         tempflat.push_back(q);
       }		
       k.push_back(tempflat);
@@ -606,7 +679,7 @@ void write_root(std::map<size_t,std::vector<Long64_t> > Matches,
 
   for (ULong64_t t = 0; t<nentries;t++){ //Event # is key used in map <Matches>
 
-  /* fprintf(stderr,"%d: Writing for event number %i  \n",__LINE__,t); */
+    /* fprintf(stderr,"%d: Writing for event number %i  \n",__LINE__,t); */
     hi_tree->GetEntry(t);
 
     if (t < Matches.size()) {
@@ -621,7 +694,8 @@ void write_root(std::map<size_t,std::vector<Long64_t> > Matches,
       for(size_t s = 0; s<n_mix; s++)
       {
         /* fprintf(stderr,"%d: %i>%i Filling With NAN \n",__LINE__,t,s); */
-        Mix_Events[s] = NAN;
+        /* Mix_Events[s] = NAN; */
+        Mix_Events[s] = -999;
       }
     /* if(t < Matches.size()){ */
 
@@ -666,10 +740,10 @@ int main(int argc, char *argv[])
   const char *track_skim= argv[5];
   int n_event_properties = 3; //z-vertex, multiplicity, v2 (flow)
   int n_duplicate = 1;
+
   std::map<size_t,std::vector<Long64_t> > Matches = mix_gale_shapley(root_file, hdf5_file,
       mix_start, mix_end, track_skim, n_event_properties, n_duplicate);
-  //std::map<size_t,std::vector<Long64_t> >	Matches = mix_gale_shapley(argv[1], argv[2], argv[3], argv[4],argv[5], 2, 1);
 
   write_txt(Matches,root_file,mix_start,mix_end,track_skim);
-  write_root(Matches,root_file,track_skim,mix_end-mix_start);
+  /* write_root(Matches,root_file,track_skim,mix_end-mix_start); */
 }
